8d6e34009926a62c7916e1022dc89d6faf83e354
Cherif Jazra <jazracherif@gmail.com>
2020-09-30 23:47:08 -0700

    feat: Add packaging for deploying static website to S3

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,5 +6,5 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'jazra.us.auth0.com',            // Auth0 domain
   clientId: 'MwBBaxqplwO5Vtm6Pcg5nIYKF6pG4ZNK',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://narrate-serverless-web-dev-193123xdka.s3-website-us-east-1.amazonaws.com/callback'
 }
